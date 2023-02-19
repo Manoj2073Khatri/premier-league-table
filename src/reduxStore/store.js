79e9f7dbd6d44f414d188b0e637ff87d2e0aacfd
ManojKhatri123 <97508784+ManojKhatri123@users.noreply.github.com>
2023-02-19 21:12:13 +0545

    popup modal design and data populated

--- src/reduxStore/store.js
+++ src/reduxStore/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import modalReducer from './features/modalSlice/modalSlice';
 import premereLeagueReducer from './features/premereLeagueSlice/premereLeagueSlice';
 
 export const store = configureStore({
   reducer: {
-    premierLeague: premereLeagueReducer
+    premierLeague: premereLeagueReducer,
+    modal:modalReducer,
   },
 });
