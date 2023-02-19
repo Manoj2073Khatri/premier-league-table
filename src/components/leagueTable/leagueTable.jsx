e9567b00d78871df88bab4cfa68eaffe65bac3a4
ManojKhatri123 <97508784+ManojKhatri123@users.noreply.github.com>
2023-02-19 18:36:59 +0545

    last five game results added

--- src/components/leagueTable/leagueTable.jsx
+++ src/components/leagueTable/leagueTable.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchClubsStart,fetchClubsSuccess,fetchClubsFailure} from '../../reduxStore/features/premereLeagueSlice/premereLeagueSlice';
 import Loader from '../loader/loader';
 import axios from 'axios';
+import Result from '../result/result';
 
 const LeagueTable = () => {
 
@@ -20,14 +21,15 @@ const LeagueTable = () => {
               'https://raw.githubusercontent.com/openfootball/football.json/master/2020-21/en.1.json'
             );
             const matches =await response.data.matches;
+          
+            console.log(matches);
 
             const clubs = matches.reduce((acc, match) => {
 
               const { score, team1, team2 } = match;
 
-              if (score?.ft?.length === undefined) return acc; // game not occurred
+              if (score?.ft?.length === undefined) return acc; // game has not occurred
 
-         
               const [score1, score2] = score.ft;
 
               const winner = score1 > score2 ? team1 : score2 > score1 ? team2 : 'draw';
@@ -35,23 +37,27 @@ const LeagueTable = () => {
               const loser = winner === team1 ? team2 : team1;
 
               if (!acc[winner]) {
-                acc[winner] = { name: winner, wins: 1, draws: 0, losses: 0, goalsScored: score1, goalsConceded: score2};
 
+                acc[winner] = { name: winner, wins: 1, draws: 0, losses: 0, goalsScored: score1, goalsConceded: score2,lastfivegame:['W']};
+                
               } else {
-
+                
                 acc[winner].wins++;
                 acc[winner].goalsScored += score1;
                 acc[winner].goalsConceded += score2;
+                acc[winner].lastfivegame.push('W');
               }
 
 
               if (!acc[loser]) {
 
-                acc[loser] = { name: loser, wins: 0, draws: 0, losses: 1, goalsScored: score2, goalsConceded: score1 };
+                acc[loser] = { name: loser, wins: 0, draws: 0, losses: 1, goalsScored: score2, goalsConceded: score1 ,lastfivegame:['L']};
 
               } else {
                 acc[loser].losses++;
                 acc[loser].goalsScored += score2;
+                acc[loser].goalsConceded += score1;
+                acc[loser].lastfivegame.push('L');
               }
 
 
@@ -59,20 +65,28 @@ const LeagueTable = () => {
                 acc[team1].draws++;
                 acc[team1].goalsScored += score1;
                 acc[team1].goalsConceded += score2;
+                acc[team1].lastfivegame.push('D');
+
                 acc[team2].draws++;
                 acc[team2].goalsScored += score2;
                 acc[team2].goalsConceded += score1;
+                acc[team2].lastfivegame.push('D');
               }
+             // console.log("acc: " , acc);
+
               return acc;
 
             }, {});
 
-
+             console.log("clubs: " , clubs);
             const clubsArray = Object.values(clubs).map((club) => {
               const points = club.wins * 3 + club.draws;
               const goalDifference=club.goalsScored-club.goalsConceded;
               const gamesPlayed=club.wins+club.draws+club.losses;
-              return { ...club, points,goalDifference,gamesPlayed };
+
+             // const lastfivegames=getLastFiveResults(club.name);
+
+              return { ...club, points,goalDifference,gamesPlayed};
             });
 
             clubsArray.sort((a, b) => {
@@ -80,7 +94,7 @@ const LeagueTable = () => {
                 if(a.name < b.name) return -1;
               return 0;
             });
-
+            
             dispatch(fetchClubsSuccess(clubsArray));
 
 
@@ -92,7 +106,9 @@ const LeagueTable = () => {
       }, [dispatch]);
 
 
-   console.log(clubs)
+     
+
+ 
 
   return (
     <div>
@@ -129,7 +145,7 @@ const LeagueTable = () => {
              <td>{data.goalsConceded}</td>
              <td>{data.goalDifference}</td>
              <td>{data.points}</td>
-             <td></td>
+             <td><Result result={data.lastfivegame}/></td>
          </tr>
         
             
