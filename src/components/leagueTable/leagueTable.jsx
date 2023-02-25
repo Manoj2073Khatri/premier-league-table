90856518a9df2416413fd9b1d979de6c01ae1981
Manoj2073Khatri <60803587+Manoj2073Khatri@users.noreply.github.com>
2023-02-25 18:22:59 +0545

    table,loader and result ui component made

---
+++ src/components/leagueTable/leagueTable.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './leagueTable.scss';
+const LeagueTable = () => {
+  return (
+    <div>
+        <table>
+            <tr>
+                <th>Position</th>
+                <th>Club</th>
+                <th>Played</th>
+                <th>Won</th>
+                <th>Drawn</th>
+                <th>Lost</th>
+                <th>GF</th>
+                <th>GA</th>
+                <th>GD</th>
+                <th>Points</th>
+                <th>Form</th>
+               
+            </tr>
+           
+            <tr>
+                <td>Island Trading</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+                <td>Island Trading</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+                <td>Island Trading</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+            </tr>
+           
+            </table>
+
+    </div>
+  )
+}
+
+export default LeagueTable
